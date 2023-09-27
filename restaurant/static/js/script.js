b4a0bae1b93966baae7c0b4621dfa58b78ba6ff2
Mats <matgus1983@gmail.com>
2023-09-27 13:47:11 +0000

    Move all basic work over from my crashed project.

---
+++ restaurant/static/js/script.js
@@ -0,0 +1,10 @@
+function initMap() {
+    let map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 12,
+      center: {
+        lat: 56.629253,
+        lng: 15.372672
+      }
+
+    });
+  }
