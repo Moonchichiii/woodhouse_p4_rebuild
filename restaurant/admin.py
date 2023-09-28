d80706bb55fa7893eccb653bcdbae8ace641936d
Mats <matgus1983@gmail.com>
2023-09-28 10:45:53 +0000

    updated admin, added display,search,filter

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from restaurant.models import Bookings
 
 # Register your models here.
-admin.site.register(Bookings)
+@admin.register(Bookings)
+
+class BookingsAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date', 'time', 'number_of_guests', 'phone_number', 'email')
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number')
+    list_filter = ('date', 'time', 'number_of_guests')
