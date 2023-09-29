5277efcaf5e549f112c0edf6dc38b59c01c0e596
Mats <matgus1983@gmail.com>
2023-09-29 13:57:03 +0000

    db test, failed warning, production db.

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,21 +1,19 @@
 from django.test import TestCase
 from django import forms
 from django.forms import ModelForm
-
-from .froms import BookingsForm 
+from .forms import BookingsForm
 from .models import Bookings
+from datetime import datetime
 
-from datetime import datetime, timedelta
+
 
 # Create your tests here.
-
 
 class BookingsFormTest(TestCase):
     
     def test_maximum_number_of_guests(self):
-
+        
         Bookings.objects.create(
-
             number_of_guests=15,
             date=datetime.today(),
             time= '17:00',
@@ -24,11 +22,9 @@ class BookingsFormTest(TestCase):
             address= '560 street',
             email='eva.n@gmail.com',
             phone_number= '23134124124'
-            
-            )
+        )
 
-
-    form_data = {
+        form_data = {
             'number_of_guests': 15,
             'date': datetime.today(),
             'time' :'17:00',
@@ -37,8 +33,22 @@ class BookingsFormTest(TestCase):
             'address' : '560 street',
             'email' :'eva.n@gmail.com',
             'phone_number' :'23134124124',
-    }
+        }
 
-    form = BookingsForm(data=form_data)
-    self.assertFalse(form.is_valid())
-    self.assertEqual(form.errors['__all__'], ['Sorry fully booked! Please choose another time or date.'])
+        form = BookingsForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['__all__'], ['Sorry fully booked! Please choose another time or date.'])
+
+
+class landing_page(TestCase):
+    
+    def setUp(self):
+
+
+    def test_landing_page(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed()
+
+
